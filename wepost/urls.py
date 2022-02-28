3ebf046a64625c17f3ee75e3cf8076c1b0c46cdb
Minhao Han <hannidiot2018@gmail.com>
2022-02-28 22:33:43 +0000

    add picture to album card

--- wepost/urls.py
+++ wepost/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 from wepost_main.views import test
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('', test, name='test'),
     path('admin/', admin.site.urls),
     path('wepost/', include('wepost_main.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
