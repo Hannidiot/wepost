7eed696a3d541029d84a6a16344c9379e0c7cbb2
Minhao Han <hannidiot2018@gmail.com>
2022-02-28 23:57:35 +0000

    add tests for base page and album_body page, all fail, to refactor

--- wepost_main/tests.py
+++ wepost_main/tests.py
@@ -5,7 +5,33 @@ class TestPageTest(TestCase):
 
     def test_page_returns_correct_html(self):
         response = self.client.get('/wepost/test/')
-        html = response.content.decode('utf8')
 
-        self.assertIn("A Test Page appears", html)
+        self.assertContains("A Test Page appears", response)
         self.assertTemplateUsed(response, 'wepost_main/test.html')
+
+
+class BasePageTest(TestCase):
+    
+    def setUp(self):
+        self.response = self.client.get('/wepost/home/')
+
+    def test_default_title(self):
+        self.assertContains('Wepost - Post Your Life Moments', self.response)
+
+    def test_default_content(self):
+        self.assertContains('Place body content here!', self.response)
+
+
+class ExploreTest(TestCase):
+
+    def test_page_returns_correct_html(self):
+        response = self.client.get('/wepost/explore/')
+        self.assertTemplateUsed(response, 'wepost_main/explore.html')
+
+    def test_navbar_show_correctly(self):
+        response = self.client.get('/wepost/explore/')
+
+        self.assertContains("nav", response)
+        self.assertContains("Wepost", response)
+
+        self.assertContains("Login", response)
