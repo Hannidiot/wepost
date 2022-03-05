e437a5eda0fdeaec5f648411022532048783a8c5
Hanliang Cai <2527345c@student.gla.ac.uk>
2022-03-05 02:20:45 +0000

    add submit header_img function and add profile page and modify personal profile function and created userprofile model

--- signuser/forms.py
+++ signuser/forms.py
@@ -7,4 +7,4 @@ from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["password","username", "email", "first_name", "last_name", "date_joined"]
+        fields = ["password","username", "email", "first_name", "last_name"]
